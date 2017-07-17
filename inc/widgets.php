34cdfd9a01d7c046e4b51d2a95bad91473ea24cf
S.M. Rafiz <rafiz@codexin.com>
2017-07-18 02:11:09 +0600

    Some Modifications in Social Widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -866,54 +866,65 @@ class Reveal_Social_Widget extends WP_Widget {
 	// back-end display of widget
 	public function form( $instance ) {
 
-		echo '<p>'. esc_html__('In Order To Use This Widget Please Fill Up The Social Profile Information In The Social Media Section of ', 'reveal') .'<strong><a href="'. esc_url(admin_url().'admin.php?page=Reveal') .'" target="_blank">'. esc_html('Reveal Options.', 'reveal') .'</a></strong></p>';
+		echo '<p>'. esc_html__('In Order To Use This Widget Please Fill Up The Social Profile Information In The "Social Media" Section of ', 'reveal') .'<strong><a href="'. esc_url(admin_url().'admin.php?page=reveal') .'" target="_blank">'. esc_html('Reveal Options.', 'reveal') .'</a></strong></p>';
 
-		echo '<p>'. esc_html('Choose The Social Profiles to be Shown:', 'reveal') .'</p>'
+		echo '<p>'. esc_html('Choose The Social Profiles to be Shown:', 'reveal') .'</p>';
+
+		$facebook	 		= ( !empty( $instance[ 'facebook' ] ) ? $instance[ 'facebook' ] : '' );
+		$twitter	 		= ( !empty( $instance[ 'twitter' ] ) ? $instance[ 'twitter' ] : '' );
+		$instagram	 		= ( !empty( $instance[ 'instagram' ] ) ? $instance[ 'instagram' ] : '' );
+		$pinterest	 		= ( !empty( $instance[ 'pinterest' ] ) ? $instance[ 'pinterest' ] : '' );
+		$behance	 		= ( !empty( $instance[ 'behance' ] ) ? $instance[ 'behance' ] : '' );
+		$google_plus	 	= ( !empty( $instance[ 'google_plus' ] ) ? $instance[ 'google_plus' ] : '' );
+		$youtube	 		= ( !empty( $instance[ 'youtube' ] ) ? $instance[ 'youtube' ] : '' );
+		$skype	 			= ( !empty( $instance[ 'skype' ] ) ? $instance[ 'skype' ] : '' );
+		$linkedin	 		= ( !empty( $instance[ 'linkedin' ] ) ? $instance[ 'linkedin' ] : '' );
+
 
 	?>
 		
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'facebook' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'facebook' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'facebook' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $facebook, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'facebook' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'facebook' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'facebook' ) ); ?>"><?php _e('Facebook', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'twitter' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'twitter' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'twitter' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $twitter, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'twitter' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'twitter' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'twitter' ) ); ?>"><?php _e('Twitter', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'instagram' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'instagram' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'instagram' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instagram, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'instagram' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'instagram' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'instagram' ) ); ?>"><?php _e('Instagram', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'pinterest' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'pinterest' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'pinterest' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $pinterest, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'pinterest' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'pinterest' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'pinterest' ) ); ?>"><?php _e('Pinterest', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'behance' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'behance' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'behance' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $behance, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'behance' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'behance' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'behance' ) ); ?>"><?php _e('Behance', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'google_plus' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'google_plus' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'google_plus' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $google_plus, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'google_plus' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'google_plus' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'google_plus' ) ); ?>"><?php _e('Google Plus', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'youtube' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'youtube' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'youtube' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $youtube, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'youtube' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'youtube' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'youtube' ) ); ?>"><?php _e('Youtube', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'skype' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'skype' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'skype' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $skype, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'skype' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'skype' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'skype' ) ); ?>"><?php _e('Skype', 'reveal'); ?></label>
 		</p>
 
 		<p style="width: 33%; float:left">
-		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $instance[ 'linkedin' ], 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'linkedin' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'linkedin' ) ); ?>" /> 
+		    <input class="checkbox" type="checkbox" <?php esc_attr( checked( $linkedin, 'on' ) ); ?> id="<?php echo esc_attr ($this->get_field_id( 'linkedin' ) ); ?>" name="<?php echo esc_attr($this->get_field_name( 'linkedin' ) ); ?>" /> 
 		    <label for="<?php echo esc_attr($this->get_field_id( 'linkedin' ) ); ?>"><?php _e('Linked In', 'reveal'); ?></label>
 		</p>
 
@@ -928,9 +939,8 @@ class Reveal_Social_Widget extends WP_Widget {
 		$instance = $old_instance;
 
         foreach ( array( 'facebook', 'twitter', 'instagram', 'pinterest', 'behance', 'google_plus', 'youtube', 'skype', 'linkedin' ) as $key => $value ) {
-            $instance[$value] = sanitize_text_field( $new_instance[$value] );
+            $instance[$value] = ( !empty( $new_instance[ $value ] ) ? sanitize_text_field( $new_instance[ $value ] ) : '' );
         }
-		
 		return $instance;
 		
 	}
@@ -952,42 +962,51 @@ class Reveal_Social_Widget extends WP_Widget {
 		printf( '%s', $args['before_widget'] ); ?>
 
 		<div class="footer-left">
-			<p><span class="italic">Follow Us:</span>
+			<p><span class="italic"><?php echo esc_html__('Follow Us:', 'reveal') ?></span>
 
 				<?php if( !empty( $cx_facebook ) && ( 'on' == $instance[ 'facebook' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_facebook); ?>"><i class="fa fa-facebook"></i></a>
+				<?php elseif( empty( $cx_facebook ) && ( 'on' == $instance[ 'facebook' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_twitter ) && ( 'on' == $instance[ 'twitter' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_twitter); ?>"><i class="fa fa-twitter"></i></a>
+				<?php elseif( empty( $cx_twitter ) && ( 'on' == $instance[ 'twitter' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_instagram ) && ( 'on' == $instance[ 'instagram' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_instagram); ?>"><i class="fa fa-instagram"></i></a>
+				<?php elseif( empty( $cx_instagram ) && ( 'on' == $instance[ 'instagram' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_pinterest ) && ( 'on' == $instance[ 'pinterest' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_pinterest); ?>"><i class="fa fa-pinterest"></i></a>
+				<?php elseif( empty( $cx_pinterest ) && ( 'on' == $instance[ 'pinterest' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_behance ) && ( 'on' == $instance[ 'behance' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_behance); ?>"><i class="fa fa-behance"></i></a>
+				<?php elseif( empty( $cx_behance ) && ( 'on' == $instance[ 'behance' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_gplus ) && ( 'on' == $instance[ 'google_plus' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_gplus); ?>"><i class="fa fa-google-plus"></i></a>
+				<?php elseif( empty( $cx_google_plus ) && ( 'on' == $instance[ 'google_plus' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_youtube ) && ( 'on' == $instance[ 'youtube' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_youtube); ?>"><i class="fa fa-youtube-play"></i></a>
+				<?php elseif( empty( $cx_youtube ) && ( 'on' == $instance[ 'youtube' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_skype ) && ( 'on' == $instance[ 'skype' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_skype); ?>"><i class="fa fa-skype"></i></a>
+				<?php elseif( empty( $cx_skype ) && ( 'on' == $instance[ 'skype' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 				<?php if( !empty( $cx_linkedin ) && ( 'on' == $instance[ 'linkedin' ] ) ) : ?>
 				<a href="<?php echo esc_url($cx_linkedin); ?>"><i class="fa fa-linkedin"></i></a>
+				<?php elseif( empty( $cx_linkedin ) && ( 'on' == $instance[ 'linkedin' ] ) ): echo '<i class="fa fa-exclamation-triangle" title="'. esc_html__('No Valid URL Found', 'reveal') .'"></i>'; ?>
 				<?php endif; ?>
 
 			</p>
