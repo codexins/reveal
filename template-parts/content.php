45f38d62fa1d429eab9dcf658479f1cf9fcbf0d7
codexmrashid <mrashid@codexin.com>
2017-07-24 13:29:55 +0600

    some blog modify

--- template-parts/content.php
+++ template-parts/content.php
@@ -54,7 +54,7 @@
 
         <footer id="entry_footer">
         <?php if(has_tag()): ?>
-			<?php the_tags('Tags: &nbsp',', ',''); ?>
+			<?php the_tags('Tags: &nbsp',' ',''); ?>
         <?php endif; ?>
 
         <?php if( reveal_option( 'reveal_single_share' ) == true ): ?>
