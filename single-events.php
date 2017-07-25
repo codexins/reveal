3156bb47f49fb6bff7e2f925e4a8874a30b6d7ca
codexarman <armanahmed@codexin.com>
2017-07-25 16:40:43 +0600

    Events Single Page

--- single-events.php
+++ single-events.php
@@ -17,9 +17,9 @@ get_header(); ?>
     <div id="content" class="main-content-wrapper site-content">
         <div class="container">
             <div class="row">
-
+                <!-- Start Event Single Page -->
                 <div class="col-md-5 col-sm-6">
-                    <!-- Start Event Single Page -->
+                    
                 	<div class="single-event-details"> 
                 		<div class="event-schedule">
                 			<div class="event-date">June 17, 2017</div>
@@ -41,7 +41,7 @@ get_header(); ?>
 
                 			<!-- Start Event Organizer's Email -->
                 			<p class="info-title">Email</p>
-                			<p class="info-title-content"><a href="mailto:klassychickboutique@gmail.com "> klassychickboutique@gmail.com </a></p>
+                			<p class="info-title-content"><a href="mailto:reveal@gmail.com "> reveal@gmail.com </a></p>
                 			<!-- End Event Organizer's Email -->
 
                 		</div>
@@ -59,10 +59,36 @@ get_header(); ?>
                 			
                 		</div>
                 		<!-- End Event Schedule -->
-
-
                 	</div>
                 </div>
+
+                <div class="col-md-7 col-sm-6">
+                	<div class="single-event-description"> 
+                		<!-- Event Title -->
+                		<div class="event-title">
+                			<h2>A Taste of Pop Up</h2>
+                		</div>
+
+                		<div class="single-event-image">
+                				<img src="http://localhost/reveal-theme/wp-content/uploads/2011/07/dsc02085.jpg" class="img-responsive" alt="">
+                		</div>
+
+                		
+                		<p class="event-description">Come out for a day of fun, shopping, and mingling on Saturday, June 17 at <em>A Taste of Pop Up. </em>We have tons of unique vendors ready to “wow” you with amazing products. You DO NOT want to miss this event!</p>
+
+                		<div class="event-venue">
+                			<h4> Venue: </h4>
+                			<div class="event-venue-info">
+                				<i class="fa fa-map-marker" aria-hidden="true"></i>
+                				<span>2734 N Graham St, Charlotte, NC 28206, USA</span>
+                			</div>
+                			<div class="map"></div>
+                		</div>
+                    </div>
+
+</div>
+<!-- Start Event Single Page -->
+
 
             </div> <!-- end of row -->
         </div> <!-- end of container -->
