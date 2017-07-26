b9c1f55e4e9f121f1e3ee1d2b4b2e27c95e07141
S.M. Rafiz <rafiz@codexin.com>
2017-07-26 23:06:47 +0600

    Page transition switch integrated in theme options

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,12 +237,8 @@
     //     jQuery(".loaders").delay(800).fadeOut("slow");
     // });
 
-    $(window).on('beforeunload', function() { 
-        $('body').fadeOut('normal');
-    });
-
     $(window).on('load', function() { 
-        $('#preloader_1').delay(500).fadeOut('slow');
+        $('#preloader_1').delay(300).fadeOut('fast');
     });
 
 
