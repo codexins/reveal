9bb4517f3e0765bb4eb881ddade88d45e562dc6b
S.M. Rafiz <rafiz@codexin.com>
2017-08-05 11:47:01 +0600

    Fixes

--- page.php
+++ page.php
@@ -32,7 +32,7 @@ get_header(); ?>
 					</div><!-- #primary -->
 				</div> <!-- end of col -->
 
-				<div class="col-sm-4 col-md-3 col-md-offset-1">
+				<div class="col-sm-4 col-md-4">
 					<div id="secondary" class="widget-area" role="complementary">
 						<?php get_sidebar() ?>
 					</div><!-- #secondary -->
