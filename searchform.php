b0dd8d6161453f2b1b332eeeed9f6d5daf846ee1
S.M. Rafiz <rafiz@codexin.com>
2017-08-24 10:48:14 +0600

    Reverted searchform

--- searchform.php
+++ searchform.php
@@ -1,6 +1,4 @@
     	<form role="search" method="get" class="input-group" action="<?php echo home_url('/'); ?>">
-			<input autocomplete="off" type="search" class="form-control ajax-search" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" title="Search" />
+			<input type="search" class="form-control" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" title="Search" />
 			<input type="submit" value="search">
-			<div class="clearable"><i class="fa fa-times"></i></div>
-			<div class="ajax-results"></div>
     	</form>
