c05d013210d31c0f907f24ea5f752b7c0bdd48da
Zaman <aktarzaman84@gmail.com>
2017-08-27 22:03:39 +0600

    changes in page templates

--- page-templates/page-full-width.php
+++ page-templates/page-full-width.php
@@ -1,39 +1,28 @@
 <?php
 /**
- * Template Name: Page - Full Width 
+ * Template Name: Page - Full Width
+ *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package reveal
  */
 
 get_header(); ?>
+	<div class="inside-page">
 
-	<div id="content" class="main-content-wrapper">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
+    <?php
+    if ( have_posts() ) :
 
-					<div id="primary" class="site-main inside-page">
-						<?php
-						if ( have_posts() ) :
-							
-							while ( have_posts() ) : the_post();
-								get_template_part( 'template-parts/content', 'page' );
+        /* Start the Loop */
+        while ( have_posts() ) : the_post();
 
-							endwhile;
-						endif; ?>
+            get_template_part( 'template-parts/content', 'page' );
 
-					</div><!-- #primary -->
 
-					<?php 
-					if( reveal_option( 'reveal_post_comments' ) ):
-						comments_template('', true);
-					endif; ?>
-					
-				</div> <!-- end of col -->
+        endwhile; 
 
-			</div> <!-- end of row -->
-		</div> <!-- end of container -->
-	</div> <!-- end of #content -->
+    endif; ?>
+
+   </div> 
 
 <?php get_footer(); ?>
---
+++ page-templates/page-no-sidebar.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Page - No Sidebar 
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package reveal
+ */
+
+get_header(); ?>
+
+	<div id="content" class="main-content-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+
+					<div id="primary" class="site-main inside-page">
+						<?php
+						if ( have_posts() ) :
+							
+							while ( have_posts() ) : the_post();
+								get_template_part( 'template-parts/content', 'page' );
+
+							endwhile;
+						endif; ?>
+
+					</div><!-- #primary -->
+
+					<?php 
+					if( reveal_option( 'reveal_post_comments' ) ):
+						comments_template('', true);
+					endif; ?>
+					
+				</div> <!-- end of col -->
+
+			</div> <!-- end of row -->
+		</div> <!-- end of container -->
+	</div> <!-- end of #content -->
+
+<?php get_footer(); ?>
