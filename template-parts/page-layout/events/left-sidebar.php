f7676e6b2e29c3315a39afd68375de95b8f829f7
Zaman <aktarzaman84@gmail.com>
2017-10-02 21:23:29 +0600

    events-list-sidebar

--- template-parts/page-layout/events/left-sidebar.php
+++ template-parts/page-layout/events/left-sidebar.php
@@ -1,18 +1,18 @@
 				<div class="col-sm-4 col-md-3">
-					<div id="secondary" class="widget-area" role="complementary" itemscope itemtype="http://schema.org/WPSideBar">
+					<div id="secondary" class="widget-area">
 						<?php get_sidebar() ?>
 					</div><!-- #secondary -->
 				</div> <!-- end of col -->
 				
 				<div class="col-sm-8 col-md-8 col-md-offset-1">
 					<div id="primary" class="site-main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/WebPageElement">
-						<?php if( reveal_option( 'reveal_portfolio_style' ) == 'grid' ): ?>
-						<div class="portfolio-grid-wrapper">
-							<?php reveal_archive_portfolio_loop(); ?>
+						<?php if( reveal_option( 'reveal_events_style' ) == 'grid' ): ?>
+						<div class="events-grid-wrapper">
+							<?php reveal_archive_events_loop(); ?>
 						</div> <!-- end of portfolio wrapper -->
 						<?php else: ?>
-						<div class="portfolio-list-wrapper">
-							<?php reveal_archive_portfolio_loop(); ?>
+						<div class="events-list-wrapper">
+							<?php reveal_archive_events_loop(); ?>
 						</div> <!-- end of portfolio list wrapper -->
 						<?php endif; ?>
 					</div><!-- #primary -->
